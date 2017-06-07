94509a8dbf3e8501a9bdc350ac748a5bd34d7e71
EAHub <assaf.20@buckeyemail.osu.edu>
2017-06-07 17:00:43 -0400

    move vars to page 2

--- app.py
+++ app.py
@@ -10,17 +10,15 @@ def main():
 def Page1():
   return render_template('Page1.html')
 
-# vars from <form id='Page1_Form' method='post' action='Page2'> on page 1
-
-ticker_symbol = request.form['ticker_symbol']
-closing = request.form['closing']
-adj_close = request.form['adj_close']
-opening = request.form['opening']
-adj_opening = request.form['adj_opening']
-
 
 @app.route('/Page2', methods=['GET', 'POST'])
 def Page2():
+	# vars from <form id='Page1_Form' method='post' action='Page2'> on page 1
+	ticker_symbol = request.form['ticker_symbol']
+	closing = request.form['closing']
+	adj_close = request.form['adj_close']
+	opening = request.form['opening']
+	adj_opening = request.form['adj_opening']
   return render_template('Page2.html')
 
 if __name__ == '__main__':
