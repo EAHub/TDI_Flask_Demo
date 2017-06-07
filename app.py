720ba552e7ac8f891d1cdadabdb82ef41e146c90
EAHub <assaf.20@buckeyemail.osu.edu>
2017-06-07 16:49:15 -0400

    adding vars to python and print on page2

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def main():
   return redirect('/Page1')
@@ -10,6 +9,15 @@ def main():
 @app.route('/Page1', methods=['GET','POST'])
 def Page1():
   return render_template('Page1.html')
+
+# vars from <form id='Page1_Form' method='post' action='Page2'> on page 1
+
+ticker_symbol = request.form['ticker_symbol']
+closing = request.form['closing']
+adj_close = request.form['adj_close']
+opening = request.form['opening']
+adj_opening = request.form['adj_opening']
+
 
 @app.route('/Page2', methods=['GET', 'POST'])
 def Page2():
