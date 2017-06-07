c8dfa368924da12ec4f593a8b9c62e71ea8af6ca
EAHub <assaf.20@buckeyemail.osu.edu>
2017-06-07 13:43:18 -0400

    page1 and page2 only

--- app.py
+++ app.py
@@ -2,13 +2,13 @@ from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
-@app.route('/')
-def main():
-  return redirect('/Page1')
-
 @app.route('/Page1')
 def Page1():
   return render_template('Page1.html')
+
+@app.route('/Page2')
+def Page2():
+  return render_template('Page2.html')
 
 if __name__ == '__main__':
   app.run(port=33507)
