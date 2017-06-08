17a7743fa649bde515a47090ca5efe3080d7e992
EAHub <assaf.20@buckeyemail.osu.edu>
2017-06-08 11:19:02 -0400

    added missing plotting library in py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect
 from bokeh.charts import TimeSeries, show, output_file
+from bokeh.plotting import figure, output_file, show
 from bokeh.layouts import column
 from bokeh import embed
 from bokeh.resources import CDN
@@ -18,22 +19,21 @@ def Page1():
   return render_template('Page1.html')
 
 def plotter():
-	ticker_symbol=request.form['ticker_symbol']
+	# ticker_symbol=request.form['ticker_symbol']
 	# closing = request.form['closing'],
 	# adj_close = request.form['adj_close'],
 	# opening = request.form['opening'],
 	# adj_opening = request.form['adj_opening']
 
-	ts_plot = figure(title = "Time Series")
-	ts_plot.line([1,2,3,4],[2,4,6,8])
-
+	ts_plot = figure(title="line", plot_width=300, plot_height=300)
+	ts_plot.line(x=[1, 2, 3, 4, 5], y=[6, 7, 2, 4, 5])
 	return ts_plot
 
 
 @app.route('/Page2', methods=['GET', 'POST'])
 def Page2():
 	bokeh_plot = plotter()
-	script, div = embed.components(bokeh_plot)
+	script, div = components(bokeh_plot)
 	return render_template('Page2.html', ticker_symbol = request.form['ticker_symbol'], div = div, script = script)
 
 if __name__ == '__main__':
