1cea79ef68af4733e75539007720058488762011
EAHub <assaf.20@buckeyemail.osu.edu>
2017-06-08 07:46:41 -0400

    attemp code and display plot pg2

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
 from flask import Flask, render_template, request, redirect
+from bokeh.charts import TimeSeries, show, output_file
+from bokeh.layouts import column
+from bokeh.embed import autoload_static
+from bokeh.resources import CDN
+import numpy as np
+import pandas as pd
+
 
 app = Flask(__name__)
 
@@ -18,12 +25,16 @@ def plotter():
   	opening = request.form['opening'],
   	adj_opening = request.form['adj_opening']
 
-  	
+	p = figure(title="line", plot_width=300, plot_height=300)
+	p.line(x=[1, 2, 3, 4, 5], y=[6, 7, 2, 4, 5])
+	return p
 
 @app.route('/Page2', methods=['GET', 'POST'])
 def Page2():
-  return render_template('Page2.html', 
-  	ticker_symbol=request.form['ticker_symbol'])
+	time_series = plotter()
+	script, div = components(p)
+	return render_template('Page2.html', 
+		ticker_symbol=request.form['ticker_symbol'], script = script, div = div)
 
 if __name__ == '__main__':
   app.run(port=33507)
