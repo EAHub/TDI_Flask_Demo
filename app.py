65b205b812d89526e5ae031cd3e9f1e2e88dd666
EAHub <assaf.20@buckeyemail.osu.edu>
2017-06-08 05:57:06 -0400

    only pass ticker to page2

--- app.py
+++ app.py
@@ -14,11 +14,7 @@ def Page1():
 @app.route('/Page2', methods=['GET', 'POST'])
 def Page2():
   return render_template('Page2.html', 
-  	ticker_symbol=request.form['ticker_symbol'],
-  	closing = request.form['closing'],
-  	adj_close = request.form['adj_close'],
-  	opening = request.form['opening'],
-  	adj_opening = request.form['adj_opening'])
+  	ticker_symbol=request.form['ticker_symbol'])
 
 if __name__ == '__main__':
   app.run(port=33507)
