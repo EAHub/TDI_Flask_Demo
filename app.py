75c02c21e05c79ea3186983a482cdcb607b8271c
EAHub <assaf.20@buckeyemail.osu.edu>
2017-06-08 20:41:25 -0400

    put plot back in

--- app.py
+++ app.py
@@ -39,8 +39,8 @@ def plotter():
 
 	ts_plot.line(x=[1, 2, 3, 4, 5], y=[6, 7, 2, 4, 5])
 
-	#if len(API_Key) > 15:
-	#	ts_plot.line(5, color='navy')
+	if len(API_Key) > 15:
+		ts_plot.line(x=[1,5],y=[6,6] color='navy')
 
 	return ts_plot
 
